cd91ce53a75d3325f5541b02d4951be7d5d3ef63
BardiaLatifi <Bardialatifi70@gmail.com>
2024-11-27 22:22:10 +0330

    boundary added

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-// the draggable object
+// The draggable object
 let dragObj = document.getElementById("analog");
 
-// setting xOffset and yOffset to 0 to prevent jumping
+// Defining the move limit of the analog
+const boundaryLeft = 91;  // left boundary
+const boundaryRight = 161; // right boundary
+const boundaryTop = 144;  // left boundary
+const boundaryBottom = 74; // right boundary
+
+// Setting xOffset and yOffset to 0 to prevent jumping
 let xOffset = 0;
 let yOffset = 0;
 
@@ -12,8 +18,8 @@ window.onload = function() {
   // Set the default position of the analog stick
   setDefaultPosition();
 
-  document.getElementById("analog").addEventListener("mousedown", startDrag, true);
-  document.getElementById("analog").addEventListener("touchstart", startDrag, true);
+  dragObj.addEventListener("mousedown", startDrag, true);
+  dragObj.addEventListener("touchstart", startDrag, true);
   document.onmouseup = stopDrag;
   document.ontouchend = stopDrag;
 }
@@ -21,7 +27,7 @@ window.onload = function() {
 // Function to set the default position to the center of leftSide
 function setDefaultPosition() {
   const leftSide = document.getElementById("leftSide");
-  const leftSideRect = leftSide.getBoundingClientRect(); 
+  const leftSideRect = leftSide.getBoundingClientRect();
 
   // Calculate center position based on leftSide dimensions
   defaultPosition.left = leftSideRect.left + leftSideRect.width / 2 - 25; // 25 is half of analog div width
@@ -37,17 +43,16 @@ function startDrag(e) {
   e.preventDefault();
   e.stopPropagation();
   dragObj = e.target;
-  dragObj.style.position = "absolute";
-  const rect = dragObj.getBoundingClientRect();
+  const analogRect = dragObj.getBoundingClientRect();
 
   if (e.type === "mousedown") {
-    xOffset = e.clientX - rect.left;
-    yOffset = e.clientY - rect.top;
-    window.addEventListener('mousemove', dragObject, true);
+    xOffset = e.clientX - analogRect.left;
+    yOffset = e.clientY - analogRect.top;
+    window.addEventListener("mousemove", dragObject, true);
   } else if (e.type === "touchstart") {
-    xOffset = e.targetTouches[0].clientX - rect.left;
-    yOffset = e.targetTouches[0].clientY - rect.top;
-    window.addEventListener('touchmove', dragObject, true);
+    xOffset = e.targetTouches[0].clientX - analogRect.left;
+    yOffset = e.targetTouches[0].clientY - analogRect.top;
+    window.addEventListener("touchmove", dragObject, true);
   }
 }
 
@@ -57,13 +62,37 @@ function dragObject(e) {
 
   if (!dragObj) return;
 
+  let clientX, clientY;
   if (e.type === "mousemove") {
-    dragObj.style.left = e.clientX - xOffset + "px";
-    dragObj.style.top = e.clientY - yOffset + "px";
+    clientX = e.clientX;
+    clientY = e.clientY;
   } else if (e.type === "touchmove") {
-    dragObj.style.left = e.targetTouches[0].clientX - xOffset + "px";
-    dragObj.style.top = e.targetTouches[0].clientY - yOffset + "px";
+    clientX = e.targetTouches[0].clientX;
+    clientY = e.targetTouches[0].clientY;
   }
+
+  // Calculate new position
+  let newLeft = clientX - xOffset;
+  let newTop = clientY - yOffset;
+
+  // Boundary checking
+  if (newLeft < boundaryLeft) {
+    newLeft = boundaryLeft;
+  } else if (newLeft > boundaryRight) {
+    newLeft = boundaryRight;
+  }
+  if (newTop > boundaryTop) {
+    newTop = boundaryTop;
+  } else if (newTop < boundaryBottom) {
+    newTop = boundaryBottom;
+  }
+
+  // Update the position of the drag object with constraints
+  dragObj.style.left = newLeft + 'px';
+  dragObj.style.top = newTop + 'px';
+
+  console.log(newLeft);
+  console.log(newTop);
 }
 
 function stopDrag(e) {
@@ -71,7 +100,7 @@ function stopDrag(e) {
     // Reset to default position, which is in the center of leftSide
     setDefaultPosition();
     dragObj = null;
-    window.removeEventListener('mousemove', dragObject, true);
-    window.removeEventListener('touchmove', dragObject, true);
+    window.removeEventListener("mousemove", dragObject, true);
+    window.removeEventListener("touchmove", dragObject, true);
   }
 }
